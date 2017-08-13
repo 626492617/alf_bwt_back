ffd7e7ad29c714444e766e58f3770360ca4e5be2
小_哈 <626492617@qq.com>
2017-08-13 14:36:41 +0800

    002

--- src/com/alf/util/EasyUIDataPage.java
+++ src/com/alf/util/EasyUIDataPage.java
@@ -7,8 +7,8 @@ package com.alf.util;
 import java.util.*;
 public class EasyUIDataPage {
 	
-	private List<?> rows;//定义结果�?//
-	private Long total; //总条�?//
+	private List<?> rows;//定义结果�?
+	private Long total; //总条�?
 	public List<?> getRows() {
 		return rows;
 	}
